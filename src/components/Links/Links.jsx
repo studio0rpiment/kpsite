9d6e1dfa5baa1e684c80c4488deffb25fca389c1
Kevin Patton <9810201+studio0rpiment@users.noreply.github.com>
2026-04-14 11:45:55 +0200

    Update Links Order in Links.jsx

--- src/components/Links/Links.jsx
+++ src/components/Links/Links.jsx
@@ -5,13 +5,15 @@ import './Links.css';
 
 const Links = () => {
   const LINKS = [
+    { title: 'WORK', url: 'https://kpcv.cargo.site/' },
     { title: 'BA/BFA INTERACTION DESGIGN @ GWU', url: 'https://corcoran.gwu.edu/undergraduate-interaction-design' },
     { title: 'MA INTERACTION DESGIGN @ GWU', url: 'https://corcoran.gwu.edu/interaction-design-graduate' },
     // { title: 'MA INTERACTION DESGIGN @ GWU', url: 'https://corcoran.gwu.edu/kevin-patton' },
     { title: 'VIMEO', url: 'https://vimeo.com/user1198196' },
     { title: 'BANDCAMP', url: 'https://kevinpatton.bandcamp.com/' },
     // { title: 'TWITTER', url: 'https://twitter.com/example' },
-    { title: 'WORK', url: 'https://kpcv.cargo.site/' },
+ 
+    
     { title: 'CONTACT', url: 'mailto:el.kevo@gmail.com' }
   ];
 
